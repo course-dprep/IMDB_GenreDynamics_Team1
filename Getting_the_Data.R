90586c86416d76ed1faf1d447362dc548bf4d3d3
timophilipse <timophilipse026@gmail.com>
2024-03-03 13:55:41 +0100

    made dummies and put filters on

--- Getting_the_Data.R
+++ Getting_the_Data.R
@@ -1,6 +1,7 @@
 #remember to turn on your university VPN. Without it the server blocks you.
 
 library(tidyverse)
+library(stringr)
 
 Data_Source <- list(
   "https://datasets.imdbws.com/title.ratings.tsv.gz",
@@ -63,27 +64,43 @@ data_sets <- list(ratings, crew, basics, episodes, akas)
 join_column <- "tconst"
 movie_data <- Reduce(function(x, y) left_join(x, y, by = join_column), data_sets)
 
-#Awesome! Now we have running code that builds an entire dataframe from seperate ones. 
-#Now let's think about some operalization basics. Or more specifically, the operalization of our variables.
+
+
+#source background job
+write_csv(movie_data, "movie_data.csv")
+print("The merged data set is saved succesfully")
+
+movie_data <- read_csv("movie_data.csv") %>% filter(numVotes> 100) %>%
+  filter(isOriginalTitle== 1) %>%
+  distinct(primaryTitle, .keep_all = TRUE) 
+  
+
+movie_data <- subset(movie_data, select = -numVotes_group)
+movie_data<- movie_data %>% filter(titleType %in% c("movie", "tvMovie", "tvMiniSeries", "tvSeries"))
+#adding dummy variable of movie vs series
+movie_data <- movie_data %>% mutate(movie_dummy= ifelse(str_detect(titleType,regex("movie", ignore_case = TRUE)), 1, 0))
+movie_data <- movie_data %>% mutate(newfilm_dummy= ifelse(startYear> 1999, 1, 0))
+
+
 
 #starting with out DV: ratings
-print(summary(movie_data$averageRating))
+summary(movie_data$averageRating)
 
 #Okay. So the scale is okay BUT remember that these are AVERAGE ratings, and that on average ratings 1 and 10 are quite extreem and frankly, unrealistic.
 #let's visualize how ratings are distributed
 
-ggplot(movie_data), aes(x = averageRating)) +
-  geom_histogram(binwidth = 1, fill = "darkorange", color = "black") +
-  labs(title = "Distribution of averageRating",
-       x = "Average Rating",y = "Frequency") +
-  scale_x_continuous(breaks = seq(0, 10, by = 1)) +  
-  theme_minimal()
+#ggplot(movie_data), aes(x = averageRating)) +
+  #geom_histogram(binwidth = 1, fill = "darkorange", color = "black") +
+  #labs(title = "Distribution of averageRating",
+   #    x = "Average Rating",y = "Frequency") +
+#  scale_x_continuous(breaks = seq(0, 10, by = 1)) +  
+#  theme_minimal()
 
-ggplot(movie_data, aes(x = averageRating)) +
-  geom_histogram(binwidth = 0.1, fill = "orange", color = "black") +
-  scale_x_continuous(breaks = seq(0, 10, by = 0.5), limits = c(0, 10), labels = scales::number_format(accuracy = 0.1)) +
-  labs(x = "averageRating", y = "Frequency") +
-  theme_minimal()
+#ggplot(movie_data, aes(x = averageRating)) +
+#  geom_histogram(binwidth = 0.1, fill = "orange", color = "black") +
+#  scale_x_continuous(breaks = seq(0, 10, by = 0.5), limits = c(0, 10), labels = scales::number_format(accuracy = 0.1)) +
+#  labs(x = "averageRating", y = "Frequency") +
+#  theme_minimal()
 
 #as you can see there appears to be a normal distribution that is slightly skewed to the left.
 #If you think logically about this this may be a result from the number of reviews given as opposed to the actual rating
@@ -91,22 +108,22 @@ ggplot(movie_data, aes(x = averageRating)) +
 print(summary(movie_data$numVotes))
 
 #do you see how tha average movie only has 106 votes? Or even 5! that will defiantely influence the average 
-ggplot(movie_data, aes(x = numVotes)) +
-  geom_dotplot(fill = "darkorange", color = "black", binwidth = 500) +
-  labs(title = "Distribution of numVotes",
-       x = "Number of Votes", y = "Frequency") + 
-  theme_minimal()
+#ggplot(movie_data, aes(x = numVotes)) +
+#  geom_dotplot(fill = "darkorange", color = "black", binwidth = 500) +
+ # labs(title = "Distribution of numVotes",
+  #     x = "Number of Votes", y = "Frequency") + 
+#  theme_minimal()
 
-movie_data$numVotes_group <- cut(movie_data$numVotes, breaks = c(-Inf, 50, Inf), labels = c("Below 50", "Above 50"))
 print(table(movie_data$numVotes_group))
+table(movie_data$titleType)
 
 
-below_100 <- movie_data %>%
-  select(numVotes) %>%
-  filter(numVotes < 100)
 
-ggplot(below_100, aes(x = numVotes)) +
-  geom_histogram() +
-  labs(title = "Distribution of numVotes Below 100",
-       x = "Group (Interval of 10)", y = "Number of Votes") +
-  theme_minimal()
+library(ggplot2)
+ggplot(movie_data,aes(x= startYear))+ geom_histogram()
+#  geom_histogram() +
+#  labs(title = "Distribution of numVotes Below 100",
+#       x = "Group (Interval of 10)", y = "Number of Votes") +
+#  theme_minimal()
+
+
