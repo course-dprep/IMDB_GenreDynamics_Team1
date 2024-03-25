e30183c6ad2864ef95f6d535e54502c3e2c5739b
Bram <b.g.w.teunissen@tilburguniversity.edu>
2024-03-25 22:19:46 +0100

    Will it ever really be final?

--- src/data_generation/getting_the_data.R
+++ src/data_generation/getting_the_data.R
@@ -2,6 +2,9 @@
 #So, remember to turn on your university VPN !
 
 library(tidyverse)
+dir.create("data")
+dir.create("data/temp", recursive = T)
+path = "data/temp/"
 
 Data_Source <- list(
   "https://datasets.imdbws.com/title.ratings.tsv.gz",
@@ -13,10 +16,9 @@ Data_Source <- list(
 # Loop through each URL in the list
 for (url in Data_Source) {
         file_name <- gsub('.*/title\\.(.*).tsv.gz', '\\1', url) # Extract the file name from the URL
-        download.file(url, destfile = file_name, mode = "wb") # Download the file
-        assign(file_name, read_tsv(file_name)) # Read the downloaded file into R
-        file.remove(file_name) # Remove the downloaded file
+        download.file(url, destfile = paste0(path, file_name), mode = "wb") # Download the file
+        assign(file_name, read_tsv(paste0(path, file_name))) # Read the downloaded file into R
+        file.remove(paste0(path, file_name)) # Remove the downloaded file
 }
 
-dir.create("data") # Create a directory to save the data
 save.image("data/movie_data.RData")
